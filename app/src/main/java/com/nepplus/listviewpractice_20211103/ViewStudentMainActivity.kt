43ea5f92c59b5efd4f4a8f7f7997e0f990772a43
김민성 <kmsz5813@naver.com>
2021-11-03 16:53:07 +0900

    학생 상세보기 화면 추가

---
+++ app/src/main/java/com/nepplus/listviewpractice_20211103/ViewStudentMainActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.listviewpractice_20211103
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStudentMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_student_main)
+    }
+}
