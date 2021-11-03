59ce5e9313635214fc37ed4e1c54c4d926996127
김민성 <kmsz5813@naver.com>
2021-11-03 17:20:07 +0900

    경고창 닫기  - 학생 삭제는 확인이 눌려야 진행되도록

--- app/src/main/java/com/nepplus/listviewpractice_20211103/MainActivity.kt
+++ app/src/main/java/com/nepplus/listviewpractice_20211103/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.nepplus.listviewpractice_20211103
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.viewpager2.adapter.StatefulAdapter
 import com.nepplus.listviewpractice_20211103.adapters.StudentAdapter
 import com.nepplus.listviewpractice_20211103.datas.StudentData
@@ -56,13 +58,28 @@ class MainActivity : AppCompatActivity() {
 
         studenListView.setOnItemLongClickListener { parent, view, position, id ->
             val longClickedStudent = mStudentList[position]
+
 //            Toast.makeText(this, "${longClickedStudent}학생이 길게 눌림", Toast.LENGTH_SHORT).show()
 
-//            길게 눌린 학생을 목록에서 제거 => 리스트뷰에서도 빠지게 해보자.
-            mStudentList.remove(longClickedStudent)
+//            정말 그 학생을 지울건지? 확인을
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("학생 삭제 확인")
+            alert.setMessage("정말 ${longClickedStudent} 학생을 제거하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+
+//                확인이 눌릴때만 실행되는 코드
+                //            길게 눌린 학생을 목록에서 제거 => 리스트뷰에서도 빠지게 해보자.
+                mStudentList.remove(longClickedStudent)
 
 //            리스트뷰 -> 어댑터에게 새로고침 시키자. (인지시키자)
-            mStudentAdapter.notifyDataSetChanged()
+                mStudentAdapter.notifyDataSetChanged()
+
+            })
+
+            alert.setNegativeButton("취소", null)
+            alert.show()
+
+
 
 
 //            boolean (true/false )을 결과로 지정해줘야함.
